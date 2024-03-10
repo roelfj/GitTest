efd453be6f4f14a9292a5b50309c3c831946d664
roelfj <roelf.jansen@gmail.com>
2024-03-10 21:32:51 +0200

    Big big big change

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, big big world!")
+            Text("Hello, big big big world!")
         }
         .padding()
     }
